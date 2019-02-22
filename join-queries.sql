bb2b1ab3aa5cf38c948a02da48a5cb66ef4b7b5f
tucker-amber <47162009+tucker-amber@users.noreply.github.com>
2019-02-22 07:15:53 -0600

    Update join-queries.sql
    Alter join queries so they are compatible with data transformations (milestone 4)

--- join-queries.sql
+++ join-queries.sql
@@ -1,40 +1,35 @@
 -- Countries that bought soybeans from US in 2000 or 2001
-SELECT DISTINCT country_code, string_field_4
-FROM `alien-grove-220420.Aggriculture.Exports_2000` 
-JOIN `alien-grove-220420.Aggriculture.Exports_2001` 
-ON country_code = string_field_4
-WHERE commodity_code LIKE '%soybean%'
+SELECT DISTINCT a.country_code
+FROM Aggriculture.Exports_2000_to_2005 a
+WHERE a.commodity_code LIKE '%soybean%'
+AND (EXTRACT(YEAR FROM a.week_ending_date) = 2000
+OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
 
--- Every unique item exported to each country
+-- Unique items exported from 2000 to 2001
 SELECT DISTINCT commodity_code, country_code  
-FROM `alien-grove-220420.Aggriculture.Exports_2000` 
-JOIN `alien-grove-220420.Aggriculture.Exports_2001` 
-ON country_code = string_field_4
+FROM Aggriculture.Exports_2000_to_2005 a
+WHERE (EXTRACT(YEAR FROM a.week_ending_date) = 2000
+OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
 
--- Every country exported to in 2000 or 2001
+-- Every country exported to from 2000 to 2001
 SELECT DISTINCT country_code 
-FROM `alien-grove-220420.Aggriculture.Exports_2000` 
-FULL JOIN `alien-grove-220420.Aggriculture.Exports_2001` 
-ON serialid = int64_field_13
+FROM Aggriculture.Exports_2000_to_2005 a 
+WHERE (EXTRACT(YEAR FROM a.week_ending_date) = 2000
+OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
 
 -- Records in 2000 or 2001 that have matching serial id's 
-SELECT DISTINCT week_ending_date, commodity_code, country_code, timestamp_field_0, string_field_1, string_field_4
-FROM `alien-grove-220420.Aggriculture.Exports_2000` 
-FULL JOIN `alien-grove-220420.Aggriculture.Exports_2001` 
-ON serialid = int64_field_13
+-- Depricated, dropped serial id from tables
 
--- All records for wheat exports in 2000 or 20001
-SELECT week_ending_date, timestamp_field_0, commodity_code, country_code, string_field_1, string_field_4
-FROM `alien-grove-220420.Aggriculture.Exports_2000` 
-FULL JOIN `alien-grove-220420.Aggriculture.Exports_2001`
-ON week_ending_date = timestamp_field_0
-WHERE commodity_code LIKE '%wheat%' OR string_field_1 LIKE '%wheat%'
-ORDER BY week_ending_date
+-- All records for wheat exports from 2000 to 20001
+SELECT a.week_ending_date, a.commodity_code, a.country_code
+FROM Aggriculture.Exports_2000_to_2005 a
+WHERE a.commodity_code LIKE '%wheat%'
+AND (EXTRACT(YEAR FROM a.week_ending_date) = 2000
+OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
 
--- Weekly export values for commodities exported to Guam in 2000 or 2001
-SELECT commodity_code, current_week_export, string_field_1, int64_field_6
-FROM `alien-grove-220420.Aggriculture.Exports_2000` 
-FULL JOIN `alien-grove-220420.Aggriculture.Exports_2001`
-ON country_code = string_field_4
-WHERE country_code LIKE '%GUAM%' OR string_field_4 LIKE '%GUAM%'
-ORDER BY week_ending_date
+-- Weekly export values for commodities exported to Guam from 2000 to 2001
+SELECT a.commodity_code, a.current_week_export
+FROM Aggriculture.Exports_2000_to_2005 a
+WHERE a.country_code like '%GUAM'
+AND (EXTRACT(YEAR FROM a.week_ending_date) = 2000
+OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
