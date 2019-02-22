984ca796b1577fffd1beae880ae6790769dde878
tucker-amber <47162009+tucker-amber@users.noreply.github.com>
2019-02-22 12:37:55 -0600

    Update join-queries.sql
    Replaced instances of "week_ending_date" with new attribute name "Date"

--- join-queries.sql
+++ join-queries.sql
@@ -2,20 +2,20 @@
 SELECT DISTINCT a.country_code
 FROM Aggriculture.Exports_2000_to_2005 a
 WHERE a.commodity_code LIKE '%soybean%'
-AND (EXTRACT(YEAR FROM a.week_ending_date) = 2000
-OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
+AND (EXTRACT(YEAR FROM a.Date) = 2000
+OR EXTRACT(YEAR FROM a.Date) = 2001)
 
 -- Unique items exported from 2000 to 2001
 SELECT DISTINCT commodity_code, country_code  
 FROM Aggriculture.Exports_2000_to_2005 a
-WHERE (EXTRACT(YEAR FROM a.week_ending_date) = 2000
-OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
+WHERE (EXTRACT(YEAR FROM a.Date) = 2000
+OR EXTRACT(YEAR FROM a.Date) = 2001)
 
 -- Every country exported to from 2000 to 2001
 SELECT DISTINCT country_code 
 FROM Aggriculture.Exports_2000_to_2005 a 
-WHERE (EXTRACT(YEAR FROM a.week_ending_date) = 2000
-OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
+WHERE (EXTRACT(YEAR FROM a.Date) = 2000
+OR EXTRACT(YEAR FROM a.Date) = 2001)
 
 -- Records in 2000 or 2001 that have matching serial id's 
 -- Depricated, dropped serial id from tables
@@ -24,12 +24,12 @@ OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
 SELECT a.week_ending_date, a.commodity_code, a.country_code
 FROM Aggriculture.Exports_2000_to_2005 a
 WHERE a.commodity_code LIKE '%wheat%'
-AND (EXTRACT(YEAR FROM a.week_ending_date) = 2000
-OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
+AND (EXTRACT(YEAR FROM a.Date) = 2000
+OR EXTRACT(YEAR FROM a.Date) = 2001)
 
 -- Weekly export values for commodities exported to Guam from 2000 to 2001
 SELECT a.commodity_code, a.current_week_export
 FROM Aggriculture.Exports_2000_to_2005 a
 WHERE a.country_code like '%GUAM'
-AND (EXTRACT(YEAR FROM a.week_ending_date) = 2000
-OR EXTRACT(YEAR FROM a.week_ending_date) = 2001)
+AND (EXTRACT(YEAR FROM a.Date) = 2000
+OR EXTRACT(YEAR FROM a.Date) = 2001)
