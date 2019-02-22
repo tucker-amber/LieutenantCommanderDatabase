6131adb0c558cd6d635a973ca8b4f628959593b9
tucker-amber <47162009+tucker-amber@users.noreply.github.com>
2019-02-22 12:36:15 -0600

    Update simple-queries.sql
    replaced instances of "week_ending_date" with new attribute name "Date"

--- simple-queries.sql
+++ simple-queries.sql
@@ -2,14 +2,14 @@
 SELECT distinct country_code
 FROM `Aggriculture.Exports_2000_to_2005` 
 WHERE commodity_code LIKE '%soybean%'
-AND EXTRACT(year FROM week_ending_date) = 2000
+AND EXTRACT(year FROM Date) = 2000
 ORDER BY country_code
 
 -- Countries (listed alphabetically) that bought soybeans from US in 2005
 SELECT distinct country_code
 FROM Aggriculture.Exports_2000_to_2005 
 WHERE commodity_code LIKE '%soybean%'
-AND EXTRACT(year FROM week_ending_date) = 2005
+AND EXTRACT(year FROM Date) = 2005
 ORDER BY country_code
 
 -- Types of food/food products purchased (listed alphabetically) from US by Finland in 2002
@@ -18,7 +18,7 @@ FROM Aggriculture.Exports_2000_to_2005 a
 LEFT JOIN Aggriculture.Countries c
 ON a.country_code = c.country_code
 WHERE c.country_name = "FINLAND"
-AND EXTRACT(year FROM a.week_ending_date) = 2002
+AND EXTRACT(year FROM a.Date) = 2002
 ORDER BY a.commodity_code
 
 -- Types of food/food products (listed alphabetically) purchased by Cuba in 2001
@@ -27,7 +27,7 @@ FROM Aggriculture.Exports_2000_to_2005 a
 LEFT JOIN Aggriculture.Countries c
 ON a.country_code = c.country_code
 WHERE c.country_name = "CUBA"
-AND EXTRACT(year FROM a.week_ending_date) = 2001
+AND EXTRACT(year FROM a.Date) = 2001
 ORDER BY a.commodity_code
 
 -- Food/Food products purchased (listed alphabetically) in 2004 by unreported countries
@@ -36,13 +36,13 @@ FROM Aggriculture.Exports_2000_to_2005 a
 LEFT JOIN Aggriculture.Countries c
 ON a.country_code = c.country_code
 WHERE c.country_name = "UNKNOWN"
-AND EXTRACT(year FROM a.week_ending_date) = 2004
+AND EXTRACT(year FROM a.Date) = 2004
 ORDER BY a.commodity_code
 
 -- List of large orders IN 2003 (identified by country name) of food/food products ordered by week (ascending)
 SELECT country_code
 FROM Aggriculture.Exports_2000_to_2005 a
 WHERE net_sales_for_week_current_year > 100000
-AND EXTRACT(year FROM a.week_ending_date) = 2003
-ORDER BY week_ending_date
+AND EXTRACT(year FROM a.Date) = 2003
+ORDER BY Date
 
