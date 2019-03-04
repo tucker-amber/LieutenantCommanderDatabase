88185d6639edd12b6b328916a66f651ebdb65f1b
tuckerar <tucker.amber.r@gmail.com>
2019-03-03 20:31:22 -0600

    added custom pardo. new table is NOT effectively created.

--- pardo_Exports_2000_to_2005.py
+++ pardo_Exports_2000_to_2005.py
@@ -33,7 +33,8 @@ class removeNumericCountryCode(beam.DoFn):
             
             # replace numeric value with string value by comparing numeric key to country code dict
             country_code = countryCodeDict.get(country_code)
-
+            
+            # Create new record with updates country_code value
             new_record = {'commodity_code': commodity_code, 'country_code': country_code, 'current_week_export': current_week_export, 'accumulated_exports_current_year': accumulated_exports_current_year, 'outstanding_sales_current_year': outstanding_sales_current_year, 'total_outstanding_sales_exports_as_of_current_week': total_outstanding_sales_exports_as_of_current_week, 'net_sales_for_week_current_year': net_sales_for_week_current_year}
             # return record with updated country_code
             return [new_record]
@@ -54,9 +55,9 @@ opts = beam.pipeline.PipelineOptions(flags=[], **options)
 # Create a Pipeline using a local runner for execution.
 with beam.Pipeline('DirectRunner', options=opts) as p:
 
-	# 1-2 make input PCollection from BigQuery query result
+    # 1-2 make input PCollection from BigQuery query result
     # Query Exports_2000_to_2005 table. selects all attributes.
-    query_results_Exports_2000_to_2005 = p | 'Read from Exports table BigQuery' >> beam.io.Read(beam.io.BigQuerySource(query='select * from Aggriculture.Exports_2000_to_2005 LIMIT 100')) # currently limiting to 100 for testing
+    query_results_Exports_2000_to_2005 = p | 'Read from Exports table BigQuery' >> beam.io.Read(beam.io.BigQuerySource(query='select * from Aggriculture.Exports_2000_to_2005 LIMIT 100')) # EDIT! currently limiting to 100 for testing
 	
     # 3 write input PCollection to local file input.txt
     query_results_Exports_2000_to_2005 | 'Write querried raw data to input.txt' >> WriteToText('input.txt')
@@ -65,10 +66,10 @@ with beam.Pipeline('DirectRunner', options=opts) as p:
     out_pcoll = query_results_Exports_2000_to_2005 | 'Replace instances of country_code where it is numeric with string country_code' >> beam.ParDo(removeNumericCountryCode())
 
 	# 5 write output PCollection to local file output.txt
-	out_pcoll | 'Write transformed data to output.txt' >> WriteToText('output.txt')
+    out_pcoll | 'Write transformed data to output.txt' >> WriteToText('output.txt')
 
-	qualified_table_name = PROJECT_ID + ':Aggriculture.test'
-	table_schema = 'commodity_code:STRING,country_code:STRING,current_week_export:INTEGER,accumulated_exports_current_year:INTEGER,outstanding_sales_current_year:INTEGER,total_outstanding_sales_exports_as_of_current_week:INTEGER,net_sales_for_week_current_year:INTEGER,date:DATE' #EDIT add schema for new table
+    qualified_table_name = PROJECT_ID + ':Aggriculture.test'
+    table_schema = 'commodity_code:STRING,country_code:STRING,current_week_export:INTEGER,accumulated_exports_current_year:INTEGER,outstanding_sales_current_year:INTEGER,total_outstanding_sales_exports_as_of_current_week:INTEGER,net_sales_for_week_current_year:INTEGER,date:DATE'
 
 	# 6 write output PCollection to new BigQuery table in main dataset
-	out_pcoll | 'Write transformed data to BigQuery' >> beam.io.Write(beam.io.BigQuerySink(qualified_table_name, schema=table_schema, create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED, write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE))
+    out_pcoll | 'Write transformed data to BigQuery' >> beam.io.Write(beam.io.BigQuerySink(qualified_table_name, schema=table_schema, create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED, write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE))
