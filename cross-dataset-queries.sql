4e1913db79a8fd90668c1fd704df3f9d47546cf5
tuckerar <tucker.amber.r@gmail.com>
2019-04-25 20:21:51 -0500

    add queries 6/6

--- cross-dataset-queries.sql
+++ cross-dataset-queries.sql
@@ -30,4 +30,18 @@ WHERE EXTRACT (YEAR FROM e.date) = 2000 or EXTRACT (YEAR FROM e.date) = 2001 or 
 GROUP BY country_code
 ORDER BY country_code
 
---
+-- Return total trade value (imports + exports) by country for years with import and exports data available
+SELECT country_code, SUM(e.current_week_export) + SUM(i.Trade_Value__US__) as Total_Trade_Value
+FROM Aggriculture.Exports_MS5 e
+FULL JOIN Imports.Imports_MS10 i ON e.country_code = upper(i.partner)
+WHERE EXTRACT (YEAR FROM e.date) = 2000 or EXTRACT (YEAR FROM e.date) = 2001 or EXTRACT (YEAR FROM e.date) = 2004 or EXTRACT (YEAR FROM e.date) = 2005
+GROUP BY country_code
+ORDER BY country_code
+
+-- Return total imports and exports by year for years with export and import data available
+SELECT year, SUM(e.current_week_export) as `Total_US_Exports`, SUM(i.Trade_Value__US__) as Total_US_Imports
+FROM Aggriculture.Exports_MS5 e
+FULL JOIN Imports.Imports_MS10 i ON EXTRACT(YEAR FROM e.date) = i.year
+WHERE year = 2000 or year = 2001 or year = 2004 or year = 2005
+GROUP BY year
+ORDER BY year
